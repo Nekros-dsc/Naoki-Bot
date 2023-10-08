cec52cea2e992f76190fd95919dcb5307204844a
burner241 <142812330+burner241@users.noreply.github.com>
2023-10-08 20:07:14 +0100

    Update config.js
    naoki v2

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     app: {
         px: '+',
-        token: '',
-        owners: [""],
+        token: 'MTE1NjIzODM3NjYzOTQxMDE3Ng.GZRVJq.tZSoHF_e6xOgu4fuGUX2ZweN6l4uumE6nN1R7E',
+        owners: ["1073003512969056376"],
         funny: '',
         color: '#2f3136',
         footer: 'Protect',
